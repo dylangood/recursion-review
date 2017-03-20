c869a74f7a0ff3a9114c81028cbbc34b061f7aba
MKS-Student <mks.student@hackreactor.com>
2017-03-20 15:36:04 -0700

    StringifiableObjects tests are passing
    Still have work to do on functions and undefined, maybe more
    after that.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,45 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var string = '';
+  // Handle unstringifiable stuff
+  //   Functions or Undefined
+  if ( 'function' === typeof obj || 'undefined' === obj ){
+    return;
+  }   
+
+  // Recurse the function for collections
+  if ( Array.isArray(obj) ) {
+    for ( var i = 0; i < obj.length; i++ ) {
+      string = string + stringifyJSON(obj[i]) + ( i < obj.length - 1  ? ',' : '' );
+    }
+    string = '[' + string + ']';
+    return string;
+  }
+
+  //   Null object requires special case
+  if( null === obj ) {
+    return 'null';
+  }
+
+  //   put commas between collection elements
+  if ( 'object' === typeof obj ) {
+    var subsequent = false;
+    for ( var k in obj ) {
+      string = string + stringifyJSON(k) + ':' + stringifyJSON(obj[k]) + ',';
+      subsequent = true;
+    }
+    string = '{' + string.slice(0, -1) + '}';
+    return string;
+  }
+
+  //   Strings need special case ""
+  if( 'string' === typeof obj ) {
+    return '"' + obj + '"';
+  }
+
+  // For easy types, just stringify
+  string += '' + obj;
+
+  return string;
 };

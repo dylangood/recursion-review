5800019e7551b10358a18569162b69e4ca294ffc
MKS-Student <mks.student@hackreactor.com>
2017-03-20 16:37:49 -0700

    All test passing for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,25 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+
+// initializing
+  var output=[]; CurrentNode=document.body;   
+//inner find function
+  function findClassInNode(node) {
+  //if node has the correct class Name
+    if ( $(node).hasClass(className) ) {
+      output.push(node);
+    }
+
+  // Recurse inner function in a for loop if there are children
+    if (node.children) {
+      for ( var i=0; i<node.children.length; i++ ) { 
+        findClassInNode( node.children[i] );
+      }
+    }
+  }
+  //call inner function on body 
+  findClassInNode(CurrentNode);
+  //Return output
+  return output;
 };
